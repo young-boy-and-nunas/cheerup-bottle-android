0e63f05dc4ecb4763a6bd49b709121c3dc61746e
jsh-me <ppm_it@naver.com>
2022-09-04 02:11:18 +0900

    add api

--- app/src/main/java/com/example/uniton4/data/repository/WorryRepository.kt
+++ app/src/main/java/com/example/uniton4/data/repository/WorryRepository.kt
@@ -3,16 +3,22 @@ package com.example.uniton4.data.repository
 import com.example.uniton4.data.ServiceApi
 import com.example.uniton4.domain.RandomWorryEntity
 import kotlinx.coroutines.flow.first
+import java.lang.Exception
 import javax.inject.Inject
 
 class WorryRepository @Inject constructor(
     private val serviceApi: ServiceApi,
     private val localRepository: LocalRepository
 ) {
-
-    suspend fun getRandomWorry(): RandomWorryEntity {
-        return serviceApi.getRandomWorry(
-            localRepository.getUserKey().first() ?: ""
-        )
+    suspend fun getRandomWorry(): Result<RandomWorryEntity> {
+        return try {
+            Result.success(
+                serviceApi.getRandomWorry(
+                    localRepository.getUserKey().first() ?: ""
+                )
+            )
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 }
