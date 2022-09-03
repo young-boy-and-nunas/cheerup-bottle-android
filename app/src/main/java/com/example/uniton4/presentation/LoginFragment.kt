42748b54b8df6dfdad06f9235135db8810fb52f3
chaeyoon <chaeyoon@linecorp.com>
2022-09-03 16:37:08 +0900

    Add fragment navigation

---
+++ app/src/main/java/com/example/uniton4/presentation/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.example.uniton4.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.uniton4.R
+
+class LoginFragment private constructor() : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = LoginFragment()
+    }
+}
